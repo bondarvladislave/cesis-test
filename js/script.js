83f46e0676c309ba4e11257f50049ea8c325f0b8
bondarvladislave <bondarvladislave@gmail.com>
2018-08-12 19:37:58 +0300

    1st commit

---
+++ js/script.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+	//Открытие меню
+	$('.header__nav-btn').click(function() {
+		$(this).toggleClass('active')
+		$('.nav-wrapper').toggle()
+		$('body').toggleClass('popup')
+	})
+ 	
+	//Закрыть меню при переходе на якорь
+	$('nav a').click(function() {
+		$('.nav-wrapper').toggle()
+		$('.header__nav-btn').toggleClass('active')
+		$('body').toggleClass('popup')
+	})
+
+	//Уменьшить высоту меню при скролле, и добавить фон
+	$(window).scroll(function() {
+		if ($(window).scrollTop() > $('.header__fixed').height()) {
+			$('.header__fixed').addClass('fixed')
+		}
+		else {
+			$('.header__fixed').removeClass('fixed')
+		}
+	})
+
+	//Фиолетовая картинка при фокусе на инпуты.
+	$('.form__input input, .footer__input input').focus(function() {
+		$(this).parent().children('div').addClass('active')
+	})	
+	$('.form__input input, .footer__input input').focusout(function() {
+		$(this).parent().children('div').removeClass('active')
+	})
+
+	//Черный текст на label если его чекбокс checked
+	$('.form__checkbox input').change(function() {
+		if ($(this).prop('checked')) {
+			$(this).parent().children('label').css('color','#2a2d2e')
+		}
+		else {
+			$(this).parent().children('label').css('color','#72787a')
+		}
+	})
+
+//плавный скролл по якорям
+  $('a').click( function() { 
+  	var target = $(this).attr('href')
+    $('html, body').animate({ scrollTop: $(target).offset().top }, 800);
+  });
+
+})
